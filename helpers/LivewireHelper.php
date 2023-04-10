26d35deacf85337bf6dd6e8282e9af6ff02d07cc
Samuel Georges <sam@daftspunk.com>
2023-04-10 12:59:15 +1000

    Fixes broken script reference when using a subfolder
    Fixes #2

--- helpers/LivewireHelper.php
+++ helpers/LivewireHelper.php
@@ -14,11 +14,15 @@ class LivewireHelper
     /**
      * renderScripts adds turbo router support, along with the baseline scripts
      */
-    public static function renderScripts()
+    public static function renderScripts($options = [])
     {
         $turboScript = Url::asset('plugins/rainlab/livewire/assets/js/livewire-turbo.js');
 
-        return Livewire::scripts() .
+        if (!isset($options['asset_url'])) {
+            $options['asset_url'] = Url::asset('');
+        }
+
+        return Livewire::scripts($options) .
             '<script src="' . $turboScript . '" data-turbo-eval="false"></script>';
     }
 }
